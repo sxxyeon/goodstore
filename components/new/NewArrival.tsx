f7f84965f18457d7cd248eacb89b44b66db57de4
suyeon <iamsxx93@gmail.com>
2024-11-04 03:19:27 +0900

    arrivals sec 수정

--- components/new/NewArrival.tsx
+++ components/new/NewArrival.tsx
@@ -2,38 +2,8 @@
 import useIsMobile from "@/hooks/useIsMobile";
 import React, { useEffect, useState } from "react";
 
-interface NewArrivals {
-  id: number;
-  image: string;
-  title: string;
-}
 const NewArrival = () => {
   const isMobile = useIsMobile();
-  const [isClient, setIsClient] = useState(false);
-  const [arrivals, setArrivals] = useState<NewArrivals[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  useEffect(() => {
-    setIsClient(true);
-    return () => {};
-  }, []);
-
-  useEffect(() => {
-    const fetchHots = async () => {
-      try {
-        const resp = await fetch(`${process.env.NEXT_PUBLIC_JSON}/arrivals`, {
-          method: "GET",
-        });
-        const json: NewArrivals[] = await resp.json();
-        setArrivals(json);
-        setLoading(false); // 데이터 로드 완료
-      } catch (error) {
-        console.error("Failed to fetch products:", error);
-        setLoading(false);
-      }
-    };
-
-    fetchHots();
-  }, []);
 
   useEffect(() => {
     const target = document.querySelector<HTMLElement>(".arrival-top");
@@ -57,17 +27,12 @@ const NewArrival = () => {
       }
     };
     window.addEventListener("scroll", scrolling);
-  }, [arrivals]);
+  }, []);
 
-  if (!isClient) {
-    return null; //서버사이드 렌더링 시에는 아무것도 렌더링 하지 않음
-  }
-
-  return arrivals.map((item) => (
+  return (
     <section
-      key={item.id}
       style={{
-        backgroundImage: `url(${item.image})`,
+        backgroundImage: `url(/img/new_arrival.png)`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
@@ -76,10 +41,10 @@ const NewArrival = () => {
       <h3
         className={`text-test transition-transform text-[8vw] font-black ease-linear translate-x-0`}
       >
-        {item.title}
+        Mantra : New Release
       </h3>
     </section>
-  ));
+  );
 };
 
 export default NewArrival;
